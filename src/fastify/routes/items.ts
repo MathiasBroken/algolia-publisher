d75ebbdc9e21e99f4c95f8dbf566064c2865d50d
Mathias Zunino <mathiaszunino@gmail.com>
2020-08-03 09:55:46 -0300

    endpoint for syncro && removing schemas folder

--- src/fastify/routes/items.ts
+++ src/fastify/routes/items.ts
@@ -1,16 +1,87 @@
-import { addItemToAlgolia } from "../services/items";
-import { Schemas } from "../schemas/index";
+import { addItemToAlgolia, updateItemAlgolia, deleteItemAlgolia, existsItemAlgolia } from "../services/items";
 
 module.exports = async function (fastify, opts) {
-  fastify.post("/", { schema: Schemas.addItemSchema }, async function (request, reply) {
-    try {
-      reply.code(200);
-      return await addItemToAlgolia(request.body);
-    } catch (error) {
-      console.trace(error);
-      reply.badRequest(error.message);
+  fastify.post(
+    "/",
+    {
+      schema: {
+        tags: ["Items"],
+        summary: "Add new item",
+        body: {
+          type: "object",
+          properties: {
+            objectID: {
+              description: "Id of the item",
+              type: "string",
+            },
+          },
+        },
+      },
+    },
+    async function (request, reply) {
+      try {
+        reply.code(200);
+        return await addItemToAlgolia(request.body);
+      } catch (error) {
+        console.trace(error);
+        reply.badRequest(error.message);
+      }
     }
-  });
+  );
+
+  fastify.put(
+    "/",
+    {
+      schema: {
+        tags: ["Items"],
+        summary: "Update item",
+        body: {
+          type: "object",
+          properties: {
+            objectID: {
+              description: "Id of the item",
+              type: "string",
+            },
+          },
+        },
+      },
+    },
+    async function (request, reply) {
+      try {
+        reply.code(200);
+        await existsItemAlgolia(request.body.objectId);
+        return await updateItemAlgolia(request.body);
+      } catch (error) {
+        console.trace(error);
+        reply.badRequest(error.message);
+      }
+    }
+  );
+
+  fastify.delete(
+    "/:itemId",
+    {
+      schema: {
+        tags: ["Items"],
+        summary: "Remove item by itemId",
+        params: {
+          itemId: {
+            description: "item id",
+          },
+        },
+      },
+    },
+    async function (request, reply) {
+      try {
+        reply.code(200);
+        await existsItemAlgolia(request.query.itemId);
+        return await deleteItemAlgolia(request.query.itemId);
+      } catch (error) {
+        console.trace(error);
+        reply.badRequest(error.message);
+      }
+    }
+  );
 };
 
 module.exports.autoPrefix = "/api/items";
--- src/fastify/services/items.ts
+++ src/fastify/services/items.ts
@@ -3,3 +3,15 @@ import { Algolia } from "../algolia";
 export async function addItemToAlgolia(item) {
   return Algolia.index.saveObject(item);
 }
+
+export async function updateItemAlgolia(item) {
+  return Algolia.index.partialUpdateObject(item);
+}
+
+export async function deleteItemAlgolia(itemId) {
+  return Algolia.index.deleteObject(itemId);
+}
+
+export async function existsItemAlgolia(itemId) {
+  return Algolia.index.getObject(itemId);
+}
